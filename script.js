eef3434fdc625f3ac979c4171b4e3d4458d99150
OvBd1 <150784897+OvBd1@users.noreply.github.com>
2024-10-25 10:44:29 +0200

    Fix: DarkMode

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const menuBurger = document.querySelector(".burger-menu");
 const navLinks = document.querySelector(".nav-links");
 
 function changeDarkMode(colorMode) {
-  if (darkMode) {
-    // Light mode
-    localStorage.setItem("darkMode", false);
-    document.documentElement.style.setProperty("--text-color", "black");
-    document.documentElement.style.setProperty("--background-color", "#efe7e5");
-    menuBurger.src = 'assets/burgerBlack.png';
-    document.getElementById("dark-light-mode").innerText = "Dark Mode";
-  } else {
-    // Dark mode
-    localStorage.setItem("darkMode", true);
-    document.documentElement.style.setProperty("--text-color", "white");
-    document.documentElement.style.setProperty("--background-color", "#333");
-    menuBurger.src = 'assets/burgerWhite.png';
-    document.getElementById("dark-light-mode").innerText = "Light Mode";
-  }
+    if (colorMode) {
+        // Light mode
+        localStorage.setItem("colorMode", false);
+        document.documentElement.style.setProperty("--text-color", "black");
+        document.documentElement.style.setProperty("--background-color", "#efe7e5");
+        menuBurger.src = 'assets/burgerBlack.png';
+        document.getElementById("dark-light-mode").innerText = "Dark Mode";
+    } else {
+        // Dark mode
+        localStorage.setItem("colorMode", true);
+        document.documentElement.style.setProperty("--text-color", "white");
+        document.documentElement.style.setProperty("--background-color", "#333");
+        menuBurger.src = 'assets/burgerWhite.png';
+        document.getElementById("dark-light-mode").innerText = "Light Mode";
+    }
 }
 
 window.onload(changeDarkMode(localStorage.getItem("colorMode")));
 
 menuBurger.addEventListener("click", () => {
-  navLinks.classList.toggle('mobile-menu');
+    navLinks.classList.toggle('mobile-menu');
 });
