23922f476df1a9170579efe72c0c514e4898061c
OvBd1 <150784897+OvBd1@users.noreply.github.com>
2024-10-25 10:31:54 +0200

    Fix: DarkMode

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const menuBurger = document.querySelector(".burger-menu");
 const navLinks = document.querySelector(".nav-links");
-let darkMode = false;
 
 function changeDarkMode(colorMode) {
-  localStorage.setItem("colorMode", true);
   if (darkMode) {
     // Light mode
-    darkMode = false;
+    localStorage.setItem("darkMode", false);
     document.documentElement.style.setProperty("--text-color", "black");
     document.documentElement.style.setProperty("--background-color", "#efe7e5");
     menuBurger.src = 'assets/burgerBlack.png';
     document.getElementById("dark-light-mode").innerText = "Dark Mode";
   } else {
     // Dark mode
-    darkMode = true;
+    localStorage.setItem("darkMode", true);
     document.documentElement.style.setProperty("--text-color", "white");
     document.documentElement.style.setProperty("--background-color", "#333");
     menuBurger.src = 'assets/burgerWhite.png';
