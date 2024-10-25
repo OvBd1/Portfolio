20aa6aa3805e6a50d83c8eaf681e1f965c9a4124
OvBd1 <150784897+OvBd1@users.noreply.github.com>
2024-10-25 11:02:15 +0200

    Fix: DarkMode

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const navLinks = document.querySelector(".nav-links");
 function changeDarkMode(colorMode) {
     if (colorMode) {
         // Light mode
-        localStorage.setItem("colorMode", false);
+        localStorage.setItem("colorMode", "false");
         document.documentElement.style.setProperty("--text-color", "black");
         document.documentElement.style.setProperty("--background-color", "#efe7e5");
         menuBurger.src = 'assets/burgerBlack.png';
         document.getElementById("dark-light-mode").innerText = "Dark Mode";
     } else {
         // Dark mode
-        localStorage.setItem("colorMode", true);
+        localStorage.setItem("colorMode", "true");
         document.documentElement.style.setProperty("--text-color", "white");
         document.documentElement.style.setProperty("--background-color", "#333");
         menuBurger.src = 'assets/burgerWhite.png';
@@ -19,7 +19,10 @@ function changeDarkMode(colorMode) {
     }
 }
 
-window.onload(changeDarkMode(localStorage.getItem("colorMode")));
+window.addEventListener("DOMContentLoaded", () => {
+    const savedColorMode = localStorage.getItem("colorMode") === "true";
+    changeDarkMode(savedColorMode);
+});
 
 menuBurger.addEventListener("click", () => {
     navLinks.classList.toggle('mobile-menu');
