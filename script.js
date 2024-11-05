830ac414adff808fee3bf6fe45e79c01ba6ed6bd
OvBd1 <150784897+OvBd1@users.noreply.github.com>
2024-11-05 14:58:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 const menuBurger = document.querySelector(".burger-menu");
 const navLinks = document.querySelector(".nav-links");
+let darkMode = false;
 
-function changeDarkMode(colorMode) {
-    if (colorMode) {
-        // Light mode
-        localStorage.setItem("colorMode", "false");
-        document.documentElement.style.setProperty("--text-color", "black");
-        document.documentElement.style.setProperty("--background-color", "#efe7e5");
-        menuBurger.src = 'assets/burgerBlack.png';
-        document.getElementById("dark-light-mode").innerText = "Dark Mode";
-    } else {
-        // Dark mode
-        localStorage.setItem("colorMode", "true");
-        document.documentElement.style.setProperty("--text-color", "white");
-        document.documentElement.style.setProperty("--background-color", "#333");
-        menuBurger.src = 'assets/burgerWhite.png';
-        document.getElementById("dark-light-mode").innerText = "Light Mode";
-    }
+function changeDarkMode() {
+  if (darkMode) {
+    // Light mode
+    darkMode = false;
+    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--background-color", "#efe7e5");
+    menuBurger.src = 'assets/burgerBlack.png';
+    document.getElementById("dark-light-mode").innerText = "Dark Mode";
+  } else {
+    // Dark mode
+    darkMode = true;
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--background-color", "#333");
+    menuBurger.src = 'assets/burgerWhite.png';
+    document.getElementById("dark-light-mode").innerText = "Light Mode";
+  }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    const savedColorMode = localStorage.getItem("colorMode") === "true";
-    changeDarkMode(savedColorMode);
-});
-
 menuBurger.addEventListener("click", () => {
-    navLinks.classList.toggle('mobile-menu');
+  navLinks.classList.toggle('mobile-menu');
 });
