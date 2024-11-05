6d85fc6f02b5671972b9fdf9e25198333f529972
OvBd1 <y.bontrond@ecole-ipssi.net>
2024-11-05 16:21:40 +0100

    Fix: DarkMode

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 const menuBurger = document.querySelector(".burger-menu");
 const navLinks = document.querySelector(".nav-links");
-let darkMode = false;
 
-function changeDarkMode() {
-  if (darkMode) {
-    // Light mode
-    darkMode = false;
-    document.documentElement.style.setProperty("--text-color", "black");
-    document.documentElement.style.setProperty("--background-color", "#efe7e5");
-    menuBurger.src = 'assets/burgerBlack.png';
-    document.getElementById("dark-light-mode").innerText = "Dark Mode";
-  } else {
-    // Dark mode
-    darkMode = true;
-    document.documentElement.style.setProperty("--text-color", "white");
-    document.documentElement.style.setProperty("--background-color", "#333");
-    menuBurger.src = 'assets/burgerWhite.png';
-    document.getElementById("dark-light-mode").innerText = "Light Mode";
-  }
+const toggleBtn = document.getElementById("toggle-btn");
+let darkMode = localStorage.getItem("dark-mode");
+
+function enableDarkMode() {
+  console.log(toggleBtn)
+  console.log('afa')
+  document.documentElement.style.setProperty("--text-color", "white");
+  document.documentElement.style.setProperty("--background-color", "#333");
+  menuBurger.src = 'assets/burgerWhite.png';
+  toggleBtn.innerText = "Light Mode";  
+  toggleBtn.classList.remove("dark-mode-toggle");
+  localStorage.setItem("dark-mode", "enabled");
+};
+ 
+function disableDarkMode() {
+  console.log(toggleBtn)
+  console.log('afa')
+  document.documentElement.style.setProperty("--text-color", "black");
+  document.documentElement.style.setProperty("--background-color", "#efe7e5");
+  menuBurger.src = 'assets/burgerBlack.png';
+  toggleBtn.innerText = "Dark Mode";
+  toggleBtn.classList.add("dark-mode-toggle");
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
+}
+
+if (darkMode === "disabled") {
+  disableDarkMode(); // set state of darkMode on page load
 }
 
 menuBurger.addEventListener("click", () => {
-  navLinks.classList.toggle('mobile-menu');
+  navLinks.classList.toggle('mobile-menu');  
 });
+
+toggleBtn.addEventListener("click", () => {
+  console.log('valide')
+  darkMode = localStorage.getItem("dark-mode");
+  
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
